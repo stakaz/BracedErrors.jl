e4da698c5f517613de8ac4d555ecf4d152fe9aa3
Stanislav Kazmin <kazmin@mis.mpg.de>
2018-08-11 16:53:14 +0100

    Added files to the Package

---
+++ src/BracedErrors.jl
@@ -0,0 +1,56 @@
+module BracedErrors
+using Formatting
+
+get_lg_int(x) = Int(floor(log10(x)))
+
+obracket = Dict(:r => "(", :s => "[", :q => "{", :a => "<", :l => "|")
+cbracket = Dict(:r => ")", :s => "]", :q => "}", :a => ">", :l => "|")
+
+"""
+		bracederror(μ::Real, σ1::Real, σ2::Real = 0.0; dec::Int = 2, suff::String = "", suff2::String = "", bracket::Symbol = :r, bracket2::Symbol = :r)
+Providing a value `μ` and one or two errors `σ1` (`σ2`) it creates a string with the values followed by the errors in brackets.
+
+This notation is commonly used in sciencific papers and this function provide an automated way of getting the appropriate string.
+
+# Parameters
+- `suff::String = ""`: optional suffix after the bracket
+- `suff2::String = ""`: optional suffix after the second bracket
+- `bracket::Symbol = :r`: type of the bracket
+- `bracket2::Symbol = :r`: type of the second bracket
+
+`brackat` and `bracket2` can take the values: $(keys(obracket)) which corespond to $(values(obracket)).
+"""
+function bracederror(μ::Real, σ1::Real, σ2::Real = 0.0; dec::Int = 2, suff::String = "", suff2::String = "", bracket::Symbol = :r, bracket2::Symbol = :r)
+	@assert σ1 > 0.0
+	@assert σ2 ≥ 0.0 ## 0.0 means no error => it will be skipped
+
+	### get the log10 power
+	μdig = get_lg_int(μ)
+	σ1dig = get_lg_int(σ1)
+	σ2dig = σ2 ≠ 0.0 ? get_lg_int(σ2) : typemax(Int)
+
+	### get the log10 power of the smaller error
+	σdig = min(σ1dig, σ2dig)
+
+	if μdig > σdig ## order of the errors smaller than order of value
+		sig = μdig - σdig + dec
+		σ1_int = Int(ceil(σ1 / 10.0^(σdig-dec+1)))
+		σ2 ≠ 0.0 && (σ2_int = Int(ceil(σ2 / 10.0^(σdig-dec+1))))
+		if μdig == sig - 1
+			val = sprintf1("%.$(sig)g", μ)
+		else
+			val = sprintf1("%#.$(sig)g", μ)
+		end
+	else ## order of the errors larger than order of value
+		σ1_int = Int(ceil(σ1))
+		σ2 ≠ 0.0 && (σ2_int = Int(ceil(σ2)))
+		val = sprintf1("%d", μ)
+	end
+	### prepare output
+	σ2_str = σ2 == 0.0 ? "" : "$(obracket[bracket2])$(σ2_int)$(cbracket[bracket2])$(suff2)"
+	σ1_str =  "$(obracket[bracket])$(σ1_int)$(cbracket[bracket])$(suff)"
+	return "$val$σ1_str$σ2_str"
+end
+
+end # BracedErrors
+
---
+++ test/runtests.jl
@@ -0,0 +1,72 @@
+using Test
+using BracedErrors
+
+@testset "get_lg_int" begin
+@test get_lg_int(12345.45) == 4
+@test get_lg_int(1.023) == 0
+@test get_lg_int(1.3e-17) == -17
+@test get_lg_int(0.004) == -3
+@test get_lg_int(1.0) == 0
+@test_throws DomainError get_lg_int(-0.1)
+@test_throws InexactError get_lg_int(0.0)
+end
+
+@testset "str_err" begin
+	@testset "dec = 2" begin
+		@test str_err(123.456, 0.12345) == "123.46(13)"
+		@test str_err(123.456, 0.0012345) == "123.4560(13)"
+		@test str_err(123.456, 0.00012345) == "123.45600(13)"
+	end
+
+	@testset "dec = 3" begin
+		@test str_err(123.456, 0.12345; dec=3) == "123.456(124)"
+		@test str_err(123.456, 0.0012345; dec=3) == "123.45600(124)"
+		@test str_err(123.456, 0.00012345; dec=3) == "123.456000(124)"
+	end
+
+	@testset "error bigger than value" begin
+		@test str_err(123.456, 123.45) == "123(124)"
+		@test str_err(123.456, 123456) == "123(123456)"
+		@test str_err(123e8, 123456e8) == "12300000000(12345600000000)"
+		@test str_err(123e-8, 123456e8) == "0(12345600000000)"
+		@test str_err(1, 123456e8) == "1(12345600000000)"
+
+		@test str_err(123.456, 123.45; dec = 4) == "123(124)"
+		@test str_err(123.456, 123456; dec = 4) == "123(123456)"
+		@test str_err(123e8, 123456e8; dec = 4) == "12300000000(12345600000000)"
+	end
+
+	@testset "two errors" begin
+		@testset "small errors" begin
+			@test str_err(123.456, 0.12345, 0.567) == "123.46(13)(57)"
+			@test str_err(123.456, 0.0012345, 78.9) == "123.4560(13)(789000)"
+			@test str_err(123.456, 12, 0.00356) == "123.4560(120000)(36)"
+		end
+
+		@testset "big errors" begin
+			@test str_err(123.456, 1234, 0.567) == "123.46(123400)(57)"
+			@test str_err(123.456, 7778, 345) == "123(7778)(345)"
+			@test str_err(1e-3, 12, 0.356) == "0(12)(1)"
+		end
+	end
+
+	@testset "at edge" begin
+		@test str_err(10.0, 1.0) == "10.0(10)"
+		@test str_err(10.0, 0.999) == "10.00(100)"
+		@test str_err(10.0, 0.999, 1.0) == "10.00(100)(100)"
+		@test str_err(10.0, 0.999, 0.344) == "10.00(100)(35)"
+	end
+
+	@testset "with e notation" begin
+		@test str_err(234.567e68, 34.6e68) == "2.35e+70(35)"
+		@test str_err(234.567e-68, 34.6e-68) == "2.35e-66(35)"
+		@test str_err(234.567e-68, 34.6e-68, 99.4e-67) == "2.35e-66(35)(994)"
+	end
+
+	@testset "styles" begin
+		@test str_err(123.456, 0.345; bracket = :s) == "123.46[35]"
+		@test str_err(123.456, 0.345; bracket = :q) == "123.46{35}"
+		@test str_err(123.456, 0.345; bracket = :a) == "123.46<35>"
+		@test str_err(123.456, 0.345; suff = "_\\inf") == "123.46(35)_\\inf"
+	end
+end
